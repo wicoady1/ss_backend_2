abb814416f6108dc947bbdbf79c407f1934c517d
Kennard <kennardwicoady@gmail.com>
2016-08-17 21:55:51 +0700

    20160817_v0001_firstcommit

---
+++ ss_backend_assess/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnBtnTestClicked (object sender, EventArgs e)
+	{
+		lblTest.LabelProp = "test";
+		btnTest.Label = "apa aja boleh";
+		//throw new NotImplementedException ();
+	}
+}
---
+++ ss_backend_assess/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace ss_backend_assess
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
