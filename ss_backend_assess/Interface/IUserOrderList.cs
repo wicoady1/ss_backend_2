f256fa4b1959bf223069299737be183d00566963
Kennard <kennardwicoady@gmail.com>
2016-08-19 15:12:15 +0700

    20160819_v0011_adminside_dev7

---
+++ ss_backend_assess/Interface/IUserOrderList.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ss_backend_assess
+{
+	public interface IUserOrderList
+	{
+	}
+}
+
---
+++ ss_backend_assess/Model/UserOrderListModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ss_backend_assess
+{
+	public class UserOrderListModel
+	{
+		public UserOrderListModel ()
+		{
+		}
+	}
+}
+
